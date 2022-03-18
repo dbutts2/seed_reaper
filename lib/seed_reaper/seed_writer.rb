9a2c2d4dc74644b9c726355c7fb90cba994bab0f
David Butts <dbutts771@gmail.com>
2022-03-18 09:40:52 -0500

    Globbing seed directory when removing

--- lib/seed_reaper/seed_writer.rb
+++ lib/seed_reaper/seed_writer.rb
@@ -7,7 +7,7 @@ module SeedReaper
     end
 
     def write!
-      FileUtils.rm_rf('db/seeds/.', secure: true)
+      FileUtils.rm_rf(Dir.glob('db/seeds/*'), secure: true)
 
       @config.each_with_index do |element, i|
         File.open("db/seeds/#{i.to_s.rjust(@config.count.digits.count, '0')}_#{file_name(element)}.seeds.rb", 'w') do |file|
